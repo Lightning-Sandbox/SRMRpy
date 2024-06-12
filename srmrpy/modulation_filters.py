b042ce6362befbfa1e4699095c73186f3aae7b8d
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2024-06-13 00:31:23 +0200

    ci/lint: enable Ruff check & format (#3)
    * enable Ruff check & format
    * fixing
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- srmrpy/modulation_filters.py
+++ srmrpy/modulation_filters.py
@@ -5,27 +5,31 @@
 # MIT license: https://github.com/jfsantos/SRMRpy/blob/master/LICENSE
 
 from __future__ import division
+
 import numpy as np
 from scipy.signal import lfilter
 
 
 def make_modulation_filter(w0, Q):
-    W0 = np.tan(w0/2)
-    B0 = W0/Q
+    W0 = np.tan(w0 / 2)
+    B0 = W0 / Q
     b = np.array([B0, 0, -B0], dtype=np.float64)
-    a = np.array([(1 + B0 + W0**2), (2*W0**2 - 2), (1 - B0 + W0**2)], dtype=np.float64)
+    a = np.array([(1 + B0 + W0**2), (2 * W0**2 - 2), (1 - B0 + W0**2)], dtype=np.float64)
     return b, a
 
+
 def modulation_filterbank(mf, fs, Q):
-    return [make_modulation_filter(w0, Q) for w0 in 2*np.pi*mf/fs]
+    return [make_modulation_filter(w0, Q) for w0 in 2 * np.pi * mf / fs]
+
 
 def compute_modulation_cfs(min_cf, max_cf, n):
-    spacing_factor = (max_cf/min_cf)**(1.0/(n-1))
+    spacing_factor = (max_cf / min_cf) ** (1.0 / (n - 1))
     cfs = np.zeros(n)
     cfs[0] = min_cf
-    for k in range(1,n):
-        cfs[k] = cfs[k-1]*spacing_factor
+    for k in range(1, n):
+        cfs[k] = cfs[k - 1] * spacing_factor
     return cfs
+
 
 def modfilt(F, x):
     y = np.zeros((len(F), len(x)), dtype=np.float64)
